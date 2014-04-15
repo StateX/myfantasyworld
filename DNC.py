a93e38cc3fe15a4884bac8e92c38620959fddcaa
StateX <statexblog@gmail.com>
2014-04-15 12:19:24 +0200

    Empezando con el proyecto
    Aprendiendo python haciendo un juego de rol xD para
    http://mihoradecodigo.blogspot.com.es/

---
+++ DNC.py
@@ -0,0 +1,11 @@
+#ciclo de dia y noche para implementarlo a mi juego. 
+for i in range(0,216000): 
+	while True:
+		 if range(0,54000): 
+			 print "Alba"
+		 elif range(54001,108000):
+			 print "Mañana"
+		 elif range(108001, 162000):
+			 print "Medio dia"
+		 elif range(162001, 216000):
+			 print "Noche"
