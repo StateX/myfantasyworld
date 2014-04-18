1df3606fabb4d509abd85aa04533cd58f3d85921
kazukunsec <jordigw@gmail.com>
2014-04-18 14:03:00 +0200

    Create mfw2.py

---
+++ mfw2.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import os 
+#Programado en spaggetticode.
+#Me gustaria que sonara alguna musica de 8bits, lo he de mirar. 
+#voy a definir todo lo que tengo pensado
+def guild(personajes): 
+	if personajes == "guild1": 
+		 print """ Nombre guild1"""
+		 print """Historia guild1"""
+		 print """ Logo ASCII guild 1"""
+		 print """Nombre miembros grupo y caracteristicas""" 
+		 d2 = raw_input("estas seguro?:" )
+		 #esto sigue pero demomento lo dejamos aquí
+	elif personajes == "guild2": 
+		 print """ Nombre guild2"""
+		 print """Historia guild2"""
+		 print """ Logo ASCII guild 2"""
+		 print """Nombre miembros grupo y caracteristicas""" 
+		 
+		 d3 = raw_input("estas seguro? :" )
+
+		 #esto sigue pero demomento lo dejamos aquí	
+	else: 
+		print "selecciona una guild"
+
+def Mapa(r,R): 
+	for i in range(r,R):
+		if range == 0 :
+			print """ | ^ | """ #vamos un pasillo	
+
+def start(juego): 
+	if d1 == "guild1":
+		print """Aventura"""
+		c1 = 0
+		c2 = 99
+		print Mapa(c1,c2)
+	elif d1 == "guild1":
+		print """Aventura2"""
+		c3 = 100
+		c4 = 199
+		print Mapa(c3,c4)		  
+
+#esto es el principio 
+print """
+		MY FANTASY WORLD
+"""
+raw_input("pulsa una tecla para empezar")
+os.system('cls')
+print "[1;31m" + "Historia del mundo"
+raw_input("pulsa una tecla para continuar")
+os.system('cls')
+print "Breve resumen histora clan"
+print "Breve explicación personajes"
+d1 =  raw_input("cual te gusta mas?: ")
+os.system('cls')
+print guild(d1)
+
+d3 = [""]
+while True:
+	d3 = raw_input("Algopor poner:")
+	if d3 == "s":
+		 print """Historia""" 
+		 print start(d1)
+	elif d3 == "n":
+		 print """elige el que mas te guste"""
+		 raw_input("Enter")
+		 os.system('cls')
+	else: 
+		print "pon s o n maldito trol del demonio"
+        break
